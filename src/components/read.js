27ba7bcb849927b0db919e23cbae5d8d66555c94
dylanlynch11850 <55436281+dylanlynch11850@users.noreply.github.com>
2022-12-05 16:25:22 +0000

    Add files via upload

---
+++ src/components/read.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Books } from "./books";
+import axios from "axios";
+
+export class Read extends React.Component{
+
+    constructor(){
+        super();
+        this.ReloadData = this.ReloadData.bind(this);
+    }
+
+    ReloadData(){
+        axios.get('http://localhost:4000/api/books')
+        .then((response)=>{
+            this.setState({books:response.data})
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
+
+    }
+    
+
+    componentDidMount() {
+        axios.get('http://localhost:4000/api/books')
+        .then((response)=>{
+            this.setState({books:response.data})
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
+    }
+
+    state = {
+        books:[ ]
+    }
+    
+    render(){
+        return(
+            <div>
+                <h3>Hello from my Read component!</h3>
+                <Books books={this.state.books} ReloadData={this.ReloadData}></Books>
+            </div>
+        );
+    }
+}
